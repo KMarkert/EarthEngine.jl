f905354a03e1eb48ef793a987c57d22e93262859
kmarkert <kel.markert@gmail.com>
2021-05-21 09:43:52 -0500

    updating doc string for Initialize function

--- src/EE.jl
+++ src/EE.jl
@@ -155,11 +155,12 @@ end
 const ee_exports = []
 
 """
-    Initialize()
+    Initialize(args...; kwargs...)
 
 Function to initialize an EarthEngine session (analagous to ee.Initialize() from
-the Python API). This function also dynamically builds the EE API and creates the
-methods with signatures for each EE Type.
+the Python API). Accepts arguments and keywords from the Python ee.Initialize()
+function. This function also dynamically builds the EE API and creates the methods 
+with signatures for each EE Type.
 """
 function Initialize(args...; kwargs...)    
     try
