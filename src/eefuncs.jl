0ffdec83896034e354c7f0e6029f3c128f48165a
kmarkert <kel.markert@gmail.com>
2021-05-22 18:20:34 -0500

    adding code to wrap ee.Algorithms functions

--- src/eefuncs.jl
+++ src/eefuncs.jl
@@ -1,5 +1,6 @@
-empty!(type_map)  # for behaving nicely in system image
+# this file create julia functions from the python modules
 
+empty!(type_map)  # for behaving nicely in system image
 
 for (ee_expr, julia_type) in pre_type_map
     type_map[ee_expr()] = julia_type
@@ -7,11 +8,11 @@ end
 
 # first loop over the ee types and set functions that take type
 for key in collect(keys(type_map))
+    # get list of methods
     methods = py"dir"(key)
-    # println(key,methods)
+    # loop over all methods and create julia methods
     for method in methods
         if ~startswith(method, "_")
-            # println(key,method)
             pyattr_set([type_map[key]], Symbol(method))
             if ~(method in ee_exports)
                 push!(ee_exports,method)
@@ -22,7 +23,7 @@ end
 
 
 # next do two passes on the individual modules
-# first pass is to get the methods
+# first pass is to get the methods for each module
 # second pass is to map the methods to julia funcs
 
 modules = [
@@ -53,19 +54,24 @@ modules = [
     :data
 ] 
 
-mod_methods = Dict()
+mod_methods = OrderedDict()
 
+# loop over the modules
 for mod in modules
     @eval begin
         submethods = collect(py"dir"(ee.$(string(mod))))
-        # get the terrain module functions
+        # get the module functions and add to dict
         mod_methods[$(string(mod))] = submethods
     end
 end
 
+# loop over the modules again
 for (k,v) in mod_methods
+    # loop over the module methods
     for submethod in v
+        # check if it is not private
         if ~startswith(submethod,"_")
+            # create a julia function of the public methods
             m = Symbol(submethod)
             @eval begin 
                 function $m(args...; kwargs...)
@@ -74,6 +80,7 @@ for (k,v) in mod_methods
                     ee_wrap(result)
                 end
             end
+            # add to list of methods for module export
             if ~(submethod in ee_exports)
                 push!(ee_exports,submethod)
             end
@@ -81,26 +88,28 @@ for (k,v) in mod_methods
     end
 end
 
+# for some reason the ee.Algorithms module is a Dict....why???
+# function to recusively search though the ee.Algorithms module and wrap all python functions
+function wrap_eealgorithms(dict)
+    for (k,v) in dict
+        if typeof(v) == PyCall.PyObject
+            m = Symbol(k)
+            @eval begin 
+                function $m(args...; kwargs...)
+                    method = $v
+                    result = pycall(method, PyObject, args...; kwargs...)
+                    ee_wrap(result)
+                end
+            end
+            # add to list of methods for module export
+            if ~(k in ee_exports)
+                push!(ee_exports,k)
+            end
+        else
+            wrap_eealgorithms(v)
+        end
+    end
+end
 
-# the modules that still need to be wrapped
-# Algorithms:
-#     CannyEdgeDetector
-#     Describe
-#     ProjectionTransform
-#     CrossCorrelation
-#     HillShadow
-#     HoughTransform
-#     ObjectType
-#     If
-#     IsEqual
-#     Terrain
-
-# FMask:
-
-# Image.Segmentation:
-
-# TemporalSegmentation:
-
-# Landsat:
-
-# Sentinel2:
+# apply recusive wrapping
+wrap_eealgorithms(ee.Algorithms)
